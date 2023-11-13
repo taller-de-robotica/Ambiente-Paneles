4e1118725049ac0a2ced0fd9bceec961cfa4de03
cocisran <fcoemmdmm@ciencias.unam.mx>
2023-11-12 19:01:27 -0600

    rutina completa

--- ambiente_panel/src/movement/movement/path_rutine.py
+++ ambiente_panel/src/movement/movement/path_rutine.py
@@ -27,6 +27,30 @@ class RobotState:
         self.rotate_z = 0
         self.w  = 0
 
+    @property
+    def deg_theta(self):
+        return abs(2*math.acos(self.w))
+
+    @property
+    def deg_rotate_x(self):
+        #TODO
+        if self.deg_theta == 0:
+            return 0
+        return self.rotate_x / math.sin(self.deg_theta)
+    
+    @property
+    def deg_rotate_y(self):
+        #TODO https://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles
+        if self.deg_theta == 0:
+            return 0
+        return self.rotate_y / math.sin(self.deg_theta)
+    
+    @property
+    def deg_rotate_z(self):
+        siny_cosp = 2 * (self.w * self.rotate_z + self.rotate_x * self.rotate_y)
+        cosy_cosp = 1 - 2 * (self.rotate_y **2 + self.rotate_z **2);        
+        return math.atan2(siny_cosp, cosy_cosp) * 180 / math.pi
+    
     def translate_magnitude(self):
         return math.sqrt(self.translate_x**2 + self.translate_y**2)
     
@@ -67,13 +91,14 @@ class PathMovement(Node):
 
         self.publisher_ = self.create_publisher(Twist, 'commands/velocity', 10)
         self.clear_buffer()
-        time.sleep(2.0)
+        time.sleep(4.0)
         print('Empezando rutina')
         self.routine = threading.Thread(target=self.make_rutine)
         self.routine.start()
 
     def position_callback(self, msg):
         self.position.update_state(msg)
+        #print(f" grados rotados eje z {self.position.deg_rotate_z}", end='\r')
         # self.get_logger().info(f"Received TF2 transform: {self.position.rotate_z} ")
 
     def image_callback(self, msg):
@@ -102,7 +127,7 @@ class PathMovement(Node):
         hit = False
         left_detector_center = (45,125)
         right_detector_center = (275,125)
-        radius = 35
+        radius = 45
 
         # Detectar colision
         for contour in final_cotours:
@@ -132,13 +157,64 @@ class PathMovement(Node):
             self.panel_hit = hit 
     
     def make_rutine(self):
-        for i in range(4):
+        
+        self.move_m(9.0,velocity=2.0)
+        time.sleep(0.4)
+        self.rotate_d(90,velocity=0.5)
+        time.sleep(0.4)
+        self.move_m(3.9,velocity=1.0)
+        time.sleep(0.4)
+        self.rotate_d(90,velocity=0.5)
+        time.sleep(0.4)
+        self.move_m(1.8,velocity=2.0)
+
+        for _ in range(9):
+            time.sleep(0.4)
+            d_to_panel = self.rotate_to_panel(direction=-1.0)
+            print(f'Gire {360 - d_to_panel} grados hasta ver un panel')
+            time.sleep(0.4)
+            self.rotate_d(d_to_panel, direction=1, velocity=0.4)
+            self.panel_hit = False
+            time.sleep(0.4)
+            self.move_m(1.8)
+
+        time.sleep(0.4)
+        self.rotate_d(90,velocity=0.5)
+        time.sleep(0.4)
+        self.move_m(4.0,velocity=2.0)
+        time.sleep(0.4)
+        self.rotate_d(90,velocity=0.5)
+        time.sleep(0.4)
+        self.move_m(1.4,velocity=1.0)
+
+        for _ in range(9):
+            time.sleep(0.4)
             d_to_panel = self.rotate_to_panel()
             print(f'Gire {d_to_panel} grados hasta ver un panel')
-            time.sleep(1.0)
-            self.rotate_d(d_to_panel, direction=-1)
+            time.sleep(0.4)
+            self.rotate_d(d_to_panel, direction=-1.0, velocity=0.4)
             self.panel_hit = False
-            time.sleep(1.0)
+            time.sleep(0.4)
+            self.move_m(1.8)
+
+
+        time.sleep(0.4)
+        self.rotate_d(90,velocity=0.5,direction=-1)
+        time.sleep(0.4)
+        self.move_m(3.7,velocity=2.0)
+        time.sleep(0.4)
+        self.rotate_d(90,velocity=0.5,direction=-1)
+        time.sleep(0.4)
+        self.move_m(1.2,velocity=1.0)
+
+        for _ in range(9):
+            time.sleep(0.4)
+            d_to_panel = self.rotate_to_panel(direction=-1.0)
+            print(f'Gire {360 - d_to_panel} grados hasta ver un panel')
+            time.sleep(0.4)
+            self.rotate_d(d_to_panel, direction=1, velocity=0.4)
+            self.panel_hit = False
+            time.sleep(0.4)
             self.move_m(1.8)
         
         
@@ -179,27 +255,27 @@ class PathMovement(Node):
 
 
     def rotate_d(self, degrees, direction = 1, velocity = 1.0):
-        z0 = self.position.rotate_z
+        z0 = self.position.deg_rotate_z
         acu = 0
         self.rotate(direction, velocity)
-        while acu * 180 < degrees:
-            acu += abs(self.position.rotate_z - z0)
-            z0 = self.position.rotate_z
-            print(f'Grados girados {acu * 180}' , end='\r')
+        while acu  < degrees:
+            acu += abs(self.position.deg_rotate_z - z0)
+            z0 = self.position.deg_rotate_z
+            print(f'Grados girados {acu}' , end='\r')
 
         self.stop_rotation()
 
-    def rotate_to_panel(self):
-        z0 = self.position.rotate_z
+    def rotate_to_panel(self, direction = 1.0):
+        z0 = self.position.deg_rotate_z 
         acu = 0
-        self.rotate(velocity=0.5)
+        self.rotate(velocity=0.5, direction=direction)
         while not self.panel_hit:
             pass
-            # acu += abs(self.position.rotate_z - z0)
-            # z0 = self.position.rotate_z
-        acu = self.position.rotate_z - z0
+            # acu += abs(self.position.deg_rotate_z - z0)
+            # z0 = self.position.deg_rotate_z
+        acu = abs(self.position.deg_rotate_z  - z0)
         self.stop_rotation()
-        return acu  * 180
+        return acu  
     
     def stop_rotation(self):
         msg = Twist()
